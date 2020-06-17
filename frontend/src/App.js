b2acf22665467a900d6c0da79a747a8849ecea9b
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-17 20:12:29 -0300

    Configurado file-loader e adicionada imagem ao App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 
+import './App.css'
+import backgroundImage from './assets/background.jpeg'
+
 import Header from './components/Header'
 
 function App() {
@@ -13,6 +16,8 @@ function App() {
   return (
     <>
       <Header title="Projects" />
+
+      <img width={150} src={backgroundImage} />
 
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
