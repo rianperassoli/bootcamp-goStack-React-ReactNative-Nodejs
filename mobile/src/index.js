f7548ad57662986a0f03110c4224469cc245b570
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-18 20:00:15 -0300

    Passa a listar os projects retornados da api

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native'
+
+import api from './services/api'
 
 const App = () => {
+
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello goStack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project} key={project.id}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   )
 }
@@ -19,12 +35,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  title: {
+  project: {
     color: '#FFF',
-    fontSize: 32,
-    fontWeight: 'bold',
+    fontSize: 30,
   }
 })
