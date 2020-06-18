92f6306f172a044c76ee235e0c28189aa0af9a41
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-18 19:32:59 -0300

    Criados estilos e organizados componentes

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
 
 const App = () => {
-  return(
-    <View>
-      <Text>Opa</Text>
-    </View>
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello goStack</Text>
+      </View>
+    </>
   )
 }
 
 export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: 'bold',
+  }
+})
