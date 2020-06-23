39d2967d51524c1260f88b8234e942d5b15db8f3
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-22 23:11:03 -0300

    Criada validação para nao adicionar o mesmo horario

--- first-project-node-typescript/src/routes/appointments.routes.ts
+++ first-project-node-typescript/src/routes/appointments.routes.ts
@@ -1,22 +1,40 @@
-import { Router } from 'express'
-import { uuid } from 'uuidv4'
+import { Router } from "express";
+import { uuid } from "uuidv4";
+import { startOfHour, parseISO, isEqual } from "date-fns";
 
-const appointmentsRouter = Router()
+const appointmentsRouter = Router();
 
-const appointments = []
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
-appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post("/", (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find((appointment) =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: "This appointment is already booked" });
+  }
 
   const appointment = {
     id: uuid(),
     provider,
-    date
-  }
+    date: parsedDate,
+  };
 
-  appointments.push(appointment)
+  appointments.push(appointment);
 
-  response.json(appointment)
-})
+  response.json(appointment);
+});
 
-export default appointmentsRouter
+export default appointmentsRouter;
--- first-project-node-typescript/src/server.ts
+++ first-project-node-typescript/src/server.ts
@@ -1,21 +1,21 @@
-import express from "express"
-import routes from "./routes"
+import express from "express";
+import routes from "./routes";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
 app.get("/", (request, response) => {
   const teste = {
     babba: 1,
     spa: 2,
     asd: 2,
-  }
+  };
 
-  return response.json({ message: "running", teste })
-})
+  return response.json({ message: "running", teste });
+});
 
 app.listen(3333, () => {
-  console.log("server started on port 3333")
-})
+  console.log("server started on port 3333");
+});
