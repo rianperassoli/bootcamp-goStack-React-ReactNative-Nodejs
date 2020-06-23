b172db167b153998be52ac05f7a200fcb6d3762d
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-22 22:05:53 -0300

    remoção de ponto e virgulas

--- first-project-node-typescript/src/server.ts
+++ first-project-node-typescript/src/server.ts
@@ -1,19 +1,19 @@
-import express from "express";
-import routes from "./routes";
+import express from "express"
+import routes from "./routes"
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 app.get("/", (request, response) => {
   const teste = {
     babba: 1,
     spa: 2,
     asd: 2,
-  };
+  }
 
-  return response.json({ message: "running", teste });
-});
+  return response.json({ message: "running", teste })
+})
 
 app.listen(3333, () => {
-  console.log("server started on port 3333");
-});
+  console.log("server started on port 3333")
+})
