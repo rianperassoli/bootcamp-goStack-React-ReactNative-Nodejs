28426ac5deda5a25c6db2e4218862b1742e54082
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-25 13:06:25 -0300

    Criado migration para campo de avatar na tabela user e configuração do multer para salvar essa imagem em disco assim que a rota recebe ela

--- iniciando-back-end/src/routes/users.routes.ts
+++ iniciando-back-end/src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express"
+import multer from "multer"
 import CreateUserService from "../services/CreateUserService"
+import ensureAutheticated from "../middlewares/ensureAuthenticated"
+import uploadConfig from "../config/upload"
 
 const usersRouter = Router()
+const upload = multer(uploadConfig)
 
 usersRouter.post("/", async (request, response) => {
   try {
@@ -18,5 +22,18 @@ usersRouter.post("/", async (request, response) => {
     return response.status(400).json({ error: err.message })
   }
 })
+
+usersRouter.patch(
+  "/avatar",
+  ensureAutheticated,
+  upload.single("avatar"),
+  async (request, response) => {
+    try {
+      return response.json({ ok: true })
+    } catch (err) {
+      return response.status(400).json({ error: err.message })
+    }
+  }
+)
 
 export default usersRouter
