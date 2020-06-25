bd7e85f74d08cd5e712b3a352bf37aad14be6e17
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-24 22:35:45 -0300

    Passa a retornar token do jwt e usuario autenticado

--- iniciando-back-end/src/services/AuthenticateUserService.ts
+++ iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -1,33 +1,41 @@
-
 import { getRepository } from "typeorm"
-import { compare } from 'bcryptjs'
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
 
 import User from "../models/User"
-
 
 interface RequestDTO {
   email: string
   password: string
 }
 
+interface ResponseDTO {
+  user: User
+  token: string
+}
+
 class AuthenticateUserService {
-  public async execute({ email, password }: RequestDTO): Promise<void> {
+  public async execute({ email, password }: RequestDTO): Promise<ResponseDTO> {
     const usersRepository = getRepository(User)
 
     const user = await usersRepository.findOne({ where: { email } })
 
     if (!user) {
-      throw new Error('Incorrect email/password combination')
+      throw new Error("Incorrect email/password combination")
     }
 
     const passwordMatched = await compare(password, user.password)
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/password combination')
+      throw new Error("Incorrect email/password combination")
     }
 
-    console.log('autenticado')
+    const token = sign({}, "bf715523de9059081f4555db3b751f95", {
+      subject: user.id,
+      expiresIn: "1d",
+    })
 
+    return { user, token }
   }
 }
 
