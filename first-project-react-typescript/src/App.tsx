56f08601b9a170111c3af60104918e152c985ce2
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-29 18:04:07 -0300

    adicionado eslint ao projeto e efetuadas configurações do eslint

--- first-project-react-typescript/src/App.tsx
+++ first-project-react-typescript/src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>App</h1>
+  </div>
+)
 
-export default App;
+export default App
