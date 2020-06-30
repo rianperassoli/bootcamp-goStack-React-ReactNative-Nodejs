fce32e28097bebfd30a969997d5c930ef2202635
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-29 22:13:34 -0300

    Criados estilos e estrutura inicial para  a pagina repository

--- first-project-react-typescript/src/pages/Repository/index.tsx
+++ first-project-react-typescript/src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { useRouteMatch } from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
+import logo from '../../assets/logo.svg'
+import { Header, RepositoryInfo, Issues } from './styles'
 
 interface RepositoryParams {
   repository: string
@@ -10,8 +14,17 @@ const Repository: React.FC = () => {
 
   return (
     <>
-      <h1>Repository</h1>
-      <h1>{params.repository}</h1>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo />
+
+      <Issues />
     </>
   )
 }
